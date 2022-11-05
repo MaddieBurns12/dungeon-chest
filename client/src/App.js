84d388c0900b121ba5f40de7a1c545d18eccdece
unknown <jaydentripp@gmail.com>
2022-11-05 16:13:08 -0400

    register and login for users

--- client/src/App.js
+++ client/src/App.js
@@ -23,3 +23,15 @@ function App() {
 }
 
 export default App;
+
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Logic goes here
+
+module.exports = app;
--- client/src/index.js
+++ client/src/index.js
@@ -15,3 +15,15 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
+
+// server listening 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
