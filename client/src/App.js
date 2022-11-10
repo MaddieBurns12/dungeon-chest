d046e9b51961ffc1396ce36e85f89ad4b16d132d
Jayden7700 <jadyentripp@gmail.com>
2022-11-09 19:37:41 -0500

    responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,26 @@ function App() {
   );
 }
 
+const isMobileDevice = useMediaQuery({
+  query: "(min-device-width: 480px)",
+});
+
+const isTabletDevice = useMediaQuery({
+  query: "(min-device-width: 768px)",
+});
+
+const isLaptop = useMediaQuery({
+  query: "(min-device-width: 1024px)",
+});
+
+const isDesktop = useMediaQuery({
+  query: "(min-device-width: 1200px)",
+});
+
+const isBigScreen = useMediaQuery({
+  query: "(min-device-width: 1201px )",
+});
+
 export default App;
 
 // require("dotenv").config();
