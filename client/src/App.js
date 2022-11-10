80e91e0ee385fc6e61ee370da35fe5ffd864182e
Madeline Burns <mburns6023@gmail.com>
2022-11-10 17:29:28 -0500

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers}) => {
-  const token = localStorage.getItem('id_toke');
+  const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
